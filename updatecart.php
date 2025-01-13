a74abaf54d6df3561283c89be5e536e8e0fe87f0
yash-251198 <shetyeyash444@gmail.com>
2025-01-13 14:18:29 +0530

    updated the updatecart code for if quantity is zero

--- updatecart.php
+++ updatecart.php
@@ -5,31 +5,24 @@ include 'db.php';
 if (empty($_SESSION['user'])) {
     header('Location:login.php');
 } else {
-
+    
     if (!empty($_GET['pid'])) {
-        if (!empty($_POST['size']) || !empty($_POST['quantity'])) {
+        $newquant = ($_POST['quantity'] <= 0) ? 1 : $_POST['quantity'];
+        $size = $_POST['size'];
+        if (!empty($newquant) && !empty($size)) {
+            echo "hi";
             $cartname = $_SESSION['user'] . "_cart";
-            $fetchcart = $con->prepare("SELECT * FROM `" . $cartname . "` WHERE pid = ?");
-            $fetchcart->bind_param("i", $_GET['pid']);
-            $fetchcart->execute();
-            $res = $fetchcart->get_result();
 
-            if ($res->num_rows > 0) {
-                $item = $res ->fetch_assoc();
-                
-                //updating the product quantity and size
-                if (!empty($_POST['quantity'] && !empty($_POST['size']))) {
-                    $newquant = $_POST['quantity'];
-                    $updtquantity = $con->prepare("UPDATE `" . $cartname . "` set quantity=?,size=? WHERE pid = ?");
-                    $updtquantity->bind_param("isi",$newquant,$_POST['size'],$_GET['pid']);
-                    
 
-                    if($updtquantity->execute()){
-                        header("Location:cart.php");
-                    }else{
-                        echo $updtquantity->error;
-                    }
-                }
+            //updating the product quantity and size
+            $updtquantity = $con->prepare("UPDATE `" . $cartname . "` set quantity=?,size=? WHERE pid = ?");
+            $updtquantity->bind_param("isi", $newquant, $size, $_GET['pid']);
+
+
+            if ($updtquantity->execute()) {
+                header("Location:cart.php");
+            } else {
+                echo $updtquantity->error;
             }
         }
     }
