5c5ab52e6fc574e9b1209ba35a6deb423c367132
yash-251198 <shetyeyash444@gmail.com>
2025-01-20 00:25:31 +0530

    solve the problem for delivery and echange date were becoming same

--- insertOrder.php
+++ insertOrder.php
@@ -7,8 +7,8 @@ if (empty($_SESSION['user'])) {
     $date = new DateTime('now');
     $orderdate = $date->format("D, d M");
     
-    $deliverydate = $date->modify('+7 days');
-    $exchangedate = $deliverydate->modify('+7 days');
+    $deliverydate = (clone $date)->modify('+7 days');
+    $exchangedate = (clone $date)->modify('+14 days');
 
     $deliverydate = $deliverydate->format("D, d M");
     $exchangedate = $exchangedate->format("D, d M");
@@ -17,7 +17,7 @@ if (empty($_SESSION['user'])) {
     $count = 0;
     foreach ($finalOrders as &$order) {
         $insertorder = $con->prepare("INSERT INTO orders_info(uid,pid,quantity,size,amt,email,contact,address,pincode,order_status,pay_meth,pay_status,order_date,delivery_date,exchangedate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $insertorder->bind_param("iiisisisiisiss", $order['uid'], $order['pid'], $order['quantity'], $order['size'], $order['amt'], $order['email'], $order['contact'], $order['address'], $order['pincode'], $order['order_status'], $order['paymeth'], $order['paystatus'], $orderdate, $deliverydate,$exchangedate);
+        $insertorder->bind_param("iiisisisiisisss", $order['uid'], $order['pid'], $order['quantity'], $order['size'], $order['amt'], $order['email'], $order['contact'], $order['address'], $order['pincode'], $order['order_status'], $order['paymeth'], $order['paystatus'], $orderdate, $deliverydate,$exchangedate);
         $insertorder->execute();
         $insertorder->close();
         $count++;
